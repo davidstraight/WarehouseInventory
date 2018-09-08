7ce07514dd80e7337f0c1ddf495b7200057e2884
David Straight <davidsstraight@gmail.com>
2018-09-07 20:47:42 -0600

    Add project files.

---
+++ WarehouseInventory/Models/WarehouseItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseInventory.Models
+{
+    class WarehouseItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Zone { get; set; }
+        public string Row { get; set; }
+        public string Shelf { get; set; }
+        p
+    }
+}
